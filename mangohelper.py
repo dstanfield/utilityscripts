840347d8c1578fbe8e15fb92aeca4d14a4a2c191
dstanfield <dstanfieldva@gmail.com>
2020-07-27 23:36:18 -0400

    adding extraction logic and fixing dumb decision

--- mangohelper.py
+++ mangohelper.py
@@ -5,8 +5,29 @@ import zipfile
 folder = r"C:\Users\dstan\Documents\testing\ziprenametesting"
 #var equals scandir checks contents of folder and returns directories only as a list
 #this for loop pattern is basically variable equals result of the for loop, iterated
-subfolders = [f.path for f in os.scandir(folder) if f.is_dir()]
+
 pgnum = 0
+zipex = ".zip"
+
+#change to directory with files
+os.chdir(folder) 
+
+#go through items in directory
+for item in os.listdir(folder): 
+    #if it's a zip
+    if item.endswith(zipex): 
+        #get the full path
+        file_name = os.path.abspath(item) 
+        #create a zipfile boi
+        zip_ref = zipfile.ZipFile(file_name) 
+        #extract it to the proper folder
+        zip_ref.extractall(folder) 
+        #close that boi
+        zip_ref.close() 
+        #credit to https://stackoverflow.com/questions/58792626/extract-all-files-from-multiple-folders-with-python
+        
+#has to be after extraction
+subfolders = [f.path for f in os.scandir(folder) if f.is_dir()]
 
 #for folder in subfolder list
 for sub in subfolders:
